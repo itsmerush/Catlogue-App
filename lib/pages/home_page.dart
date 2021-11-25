b6bd23cf292bef0183904d081d9b210e87152710
ita_me_rush <rushilokhande78@gmail.com>
2021-11-25 15:48:11 +0530

    day13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,12 +3,38 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catlog.dart';
 import 'package:flutter_application_1/pages/item_widget.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/models/catlog.dart';
+import 'package:flutter/src/services/asset_bundle.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catlogJson = await rootBundle.loadString("assets/files/catlog.json");
+    final decodedData = jsonDecode(catlogJson);
+    var productsData = decodedData["products"];
+    CatlogueModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +44,16 @@ class HomePage extends StatelessWidget {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: ListView.builder(
-        itemCount: CatlogueModel.items.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            item: CatlogueModel.items[index],
-          );
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatlogueModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CatlogueModel.items[index],
+            );
+          },
+        ),
       ),
       drawer: MyDrawer(),
     );
